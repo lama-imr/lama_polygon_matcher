8391c99cf0d9e5f790bf32b2df540101ef608971
Gaël Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2014-12-19 09:07:10 +0100

    pm_fourier: service name is compute_dissimilarity

--- pm_fourier/src/pm_fourier_node.cpp
+++ pm_fourier/src/pm_fourier_node.cpp
@@ -4,21 +4,21 @@
 
 int main(int argc, char **argv)
 {
-  int max_thread;
-
   ros::init(argc, argv, "pm_fourier_dist_server");
-  ros::NodeHandle n;
-  FourierDistance fdist(n);
-  // ros::ServiceServer service = n.advertiseService("pm_fourier_similarity", &FourierDistance::dissimilarity,&fdist);
-  ros::ServiceServer service = n.advertiseService(ros::this_node::getName(), &FourierDistance::dissimilarity, &fdist);
-  n.param<int>("max_thread", max_thread, 2);
-  ROS_INFO("%s: ready to work (with %i threads)", ros::this_node::getName().c_str(), max_thread);
-  ros::Publisher pub = n.advertise<std_msgs::String>("node_register", 10, true);
+  ros::NodeHandle nh("~");
 
-  std_msgs::String msg;
-  msg.data = ros::this_node::getName();
-  pub.publish(msg);
-  ros::spin();
+  FourierDistance fdist(nh);
+  ros::ServiceServer service = nh.advertiseService("compute_dissimilarity", &FourierDistance::dissimilarity, &fdist);
+
+  int max_thread;
+  nh.param<int>("max_thread", max_thread, 2);
+
+  ROS_INFO("Ready to work (with %i threads)", max_thread);
+  /* ros::Publisher pub = global_nh.advertise<std_msgs::String>("node_register", 10, true); */
+
+  /* std_msgs::String msg; */
+  /* msg.data = ros::this_node::getName(); */
+  /* pub.publish(msg); */
 
   ros::MultiThreadedSpinner spinner(max_thread); 
   spinner.spin();
